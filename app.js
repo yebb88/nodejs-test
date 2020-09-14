b634dc7d3d5a41a9657bd5124cc01f1774db3e64
Yair Blanco <{ID}+{username}@users.noreply.github.com>
2020-09-13 21:11:57 -0500

    Se agrega charset para caracteres especiales usados en español

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const port = 3000;
 
 const server = http.createServer((req, res) => {
 	res.statusCode = 200;
-	res.setHeader('Content-Type', 'text/plain');
+	res.setHeader('Content-Type', 'text/plain; charset=utf-8');
 	res.end('¡Hola mundo!');
 });
 
